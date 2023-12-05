6b353b3d8b2aea6b72578ee90cbd9b76b9d9d668
ronaldod3 <152335777+ronaldod3@users.noreply.github.com>
2023-12-04 22:01:25 -0300

    Add files via upload

---
+++ Projeto 3/plotter/__main__.py
@@ -0,0 +1,14 @@
+"""
+Módulo Principal
+Descrição: Este é o módulo com a função principal que integra as fases de entrada, processamento e saída de dados do Aplicativo de Plotagem.
+Autor: Letícia Santos e Ronaldo Debiasi
+Versão: 0.0.1
+Data: 04/12/2023
+
+"""
+
+# Importação dos módulos
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
---
+++ Projeto 3/plotter/es.py
@@ -0,0 +1,8 @@
+"""
+Módulo Entrada e Saída
+Descrição: Este módulo prevê funções de entrada e saída de dados para Aplicativo de Plotagem.
+Autor: Letícia Santos e Ronaldo Debiasi
+Versão: 0.0.1
+Data: 04/12/2023
+
+"""
